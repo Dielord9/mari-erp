f56fcc23f45f315e44a6be2d6a07230f9e22de75
Dielord9 <130237173+Dielord9@users.noreply.github.com>
2025-08-19 09:49:30 -0700

    Create pos.js

---
+++ frontend/pages/pos.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+
+export default function PosPage() {
+  return (
+    <Layout>
+      <h2 className="text-2xl font-semibold">POS</h2>
+      <p>POS page content goes here.</p>
+    </Layout>
+  );
+}
